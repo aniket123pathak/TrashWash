11a980d744ef5162d6e038ed2081c5d524240acd
Aniket Pathak <aniketpathak581@gmail.com>
2026-01-04 17:16:44 +0530

    report model

---
+++ Backend/src/models/report.model.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+
+const reportSchema = new Schema(
+  {
+    User: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    trashLocation: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        default: "Point",
+      },
+      coordinates: {
+        type: [Number],
+      },
+    },
+    trashImage: {
+      type: String,
+      required: true,
+    },
+    reportStatus: {
+      type: String,
+      enum: ["open", "assigned", "cleaned", "expired"],
+      default:"open"
+    },
+  },
+  { timestamps: true }
+);
+
+reportSchema.index({ trashLocation: "2dsphere" });
+
+export const Report = mongoose.model("Report", reportSchema);
